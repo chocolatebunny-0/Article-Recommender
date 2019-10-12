f312a7b721634da13481a64d7e8572ef23db15fb
sam <samndu2@gmail.com>
2019-10-12 19:30:32 +0100

    task 2

---
+++ task2_keywords.py
@@ -0,0 +1,19 @@
+''' module for getting keywords from a title '''
+from sklearn.feature_extraction.text import TfidfVectorizer
+def keywords(title):
+    ''' function that recieves title and return the keywords '''
+
+    # turn input to list to be analysed
+    title = [title]
+
+    # Remove all english stop words such as 'the', 'a'
+    tfidf = TfidfVectorizer(stop_words='english')
+
+    # #Construct the required TF-IDF matrix by tokenizing and transforming the data
+    tfidf.fit_transform(title)
+
+    # return an string of keywords from the title
+    return " ".join(tfidf.get_feature_names())
+
+# call function
+print(keywords("at a"))
